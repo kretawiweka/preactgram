cdddb0d251c1e56c0e32b32970ed782a0b2b0c7a
kretawiweka <ekanuraga@gmail.com>
2020-06-12 13:20:17 +0700

    Add material ui and themes constant

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import './style'
-import App from './components/app'
+/* eslint-disable */
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { ThemeProvider } from '@material-ui/styles'
+import App from 'components/App'
+import theme from './theme'
 
-export default App
+export default () => (
+	<div id="app">
+		<ThemeProvider theme={theme}>
+			<link
+				rel="stylesheet"
+				href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+			/>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</div>
+)
